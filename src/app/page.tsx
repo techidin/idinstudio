6f324dc25a719c8f8282718797a680bb00e42ddb
Ahmad Muhyidin <idinz.dev@gmail.com>
2025-07-03 11:39:52 +0700

    fix: update title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Idin Studio",
+  description: "This is Home for Idin Studio",
   // other metadata
 };
 
